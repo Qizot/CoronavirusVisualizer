0cfbde46f971d31504b983d22c9517cb3c1a12fa
Qizot <perzylo.jakub@gmail.com>
2020-07-05 14:08:28 +0200

    data scraper single repo setup

--- backend/coronavirus-visualizer-api/src/config/constants.ts
+++
@@ -1,4 +0,0 @@
-export const PORT = process.env.PORT || 4000;
-export const HOST = process.env.HOST || "localhost";
-export const MONGO_DB = process.env.MONGODB_URI || "mongodb://localhost/corona_visualizer";
-export const VIRUS_DATA_API = process.env.VIRUS_ATA_API || "localhost:5000";
